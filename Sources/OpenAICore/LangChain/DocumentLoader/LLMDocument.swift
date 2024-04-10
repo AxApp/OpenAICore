6ce8f44595269b262af660d55264f2520b231ebe
linhey <is.linhey@outlook.com>
2024-04-10 22:45:18 +0800

    chore: 新增 hash

--- Sources/OpenAICore/LangChain/DocumentLoader/LLMDocument.swift
+++ Sources/OpenAICore/LangChain/DocumentLoader/LLMDocument.swift
@@ -8,13 +8,14 @@
 import Foundation
 import AnyCodable
 import Tiktoken
+import Crypto
 
 public struct LLMDocument: Codable {
     
     public typealias Metadata = [String: AnyCodable]
     public var content: String
     public var metadata: Metadata
-    public init(content: String, metadata: Metadata) {
+    public init(content: String, metadata: Metadata = .init()) {
         self.content = content
         self.metadata = metadata
     }
@@ -24,6 +25,15 @@ public extension LLMDocument {
     
     func tiktoken(_ name: Tiktoken.EncodeName) async throws -> Int {
         try await Tiktoken.shared.getEncoding(name)?.encode(value: content).count ?? 0
+    }
+    
+    func hash(_ hasher: any HashFunction.Type = SHA256.self) -> String? {
+        guard let data = content.data(using: .utf8) else {
+            return nil
+        }
+        return hasher.hash(data: data)
+            .compactMap { String(format: "%02x", $0) }
+            .joined()
     }
     
 }
